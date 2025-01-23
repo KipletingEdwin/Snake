8d614f648e7437921254c0034eb081dea4ae8105
Kipleting Edwin <kipletingedwin4@gmail.com>
2025-01-23 10:23:47 +0000

    setDefaultCloseOperation to EXIT_ON_CLOSE

--- src/GameFrame.java
+++ src/GameFrame.java
@@ -5,6 +5,7 @@ public class GameFrame extends JFrame {
     GameFrame(){
         this.add(new GamePanel());
         this.setTitle("Snake");
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
     }
 }
