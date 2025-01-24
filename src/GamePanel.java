3d6f1a2d64a8c69b04d025e8e71292708b9f99e1
Kipleting Edwin <kipletingedwin4@gmail.com>
2025-01-24 20:46:56 +0000

    Add GAME_UNITS

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -9,6 +9,8 @@ public class GamePanel extends JPanel implements ActionListener {
     static  final  int SCREEN_WIDTH = 600;
     static  final  int SCREEN_HEIGHT = 600;
     static  final  int UNIT_SIZE = 25;
+    static  final  int GAME_UNITS = (SCREEN_WIDTH*SCREEN_HEIGHT)/UNIT_SIZE;
+
 
     GamePanel(){
 
