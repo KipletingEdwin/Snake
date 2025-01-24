506d94e6ec3b84868791864af55ccbdccbaf2b48
Kipleting Edwin <kipletingedwin4@gmail.com>
2025-01-24 20:58:44 +0000

    Add an array x

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -11,6 +11,7 @@ public class GamePanel extends JPanel implements ActionListener {
     static  final  int UNIT_SIZE = 25;
     static  final  int GAME_UNITS = (SCREEN_WIDTH*SCREEN_HEIGHT)/UNIT_SIZE;
     static final int DELAY = 75;
+    final int x[] = new int [GAME_UNITS];
 
 
     GamePanel(){
