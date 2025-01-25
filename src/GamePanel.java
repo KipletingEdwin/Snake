b945404e02fa120ce25eb7b814974390bcd74849
Kipleting Edwin <kipletingedwin4@gmail.com>
2025-01-25 10:20:48 +0000

    Add random

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -4,6 +4,7 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
+import java.util.Random;
 
 public class GamePanel extends JPanel implements ActionListener {
     static  final  int SCREEN_WIDTH = 600;
@@ -19,6 +20,7 @@ public class GamePanel extends JPanel implements ActionListener {
     char direction = 'R';
     boolean running = false;
     Timer timer;
+    Random random;
 
 
     GamePanel(){
