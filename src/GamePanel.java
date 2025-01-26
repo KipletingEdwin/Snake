54047d7c55bd40309d224f4bb425e66a4ea5a9d1
Kipleting Edwin <kipletingedwin4@gmail.com>
2025-01-26 09:13:59 +0000

    Draw y-axis lines

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -38,22 +38,20 @@ public class GamePanel extends JPanel implements ActionListener {
         running = true;
         timer = new Timer(DELAY,this);
         timer.start();
-
     }
 
     public void  paintComponent(Graphics g){
         super.paintComponent(g);
         draw(g);
-
     }
 
     public  void draw(Graphics g){
 
         for (int i = 0; i< SCREEN_HEIGHT/UNIT_SIZE;i++){
             g.drawLine(i*UNIT_SIZE,0,i*UNIT_SIZE,SCREEN_HEIGHT);
+            g.drawLine(0,i*UNIT_SIZE,SCREEN_WIDTH,i*UNIT_SIZE);
 
         }
-
     }
 
     public void  newApple(){
