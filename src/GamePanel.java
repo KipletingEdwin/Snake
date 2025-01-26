4db9a759c5a5cd517d24c2ffaaf25c0180f05dfc
Kipleting Edwin <kipletingedwin4@gmail.com>
2025-01-26 09:19:35 +0000

    Add int appleY variable

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -17,6 +17,7 @@ public class GamePanel extends JPanel implements ActionListener {
     int bodyParts = 6;
     int applesEaten;
     int appleX;
+    int appleY;
     char direction = 'R';
     boolean running = false;
     Timer timer;
